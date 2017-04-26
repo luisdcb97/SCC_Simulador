c16f0e73934aee0c8c7a1438fa165a2bc12fe22b
SPECTER\Luis David <luisdcb97@gmail.com>
2017-04-27 00:42:00 +0100

    Muda formatacao de floats para 5 casas decimais

--- Servico.py
+++ Servico.py
@@ -81,19 +81,11 @@ class Servico:
         # Tempo m�dio de atendimento no servi�o
         utilizacao_serv = (self.soma_temp_servico / self.simulador.tempo) / self.numero_maquinas
 
-        # Apresenta resultados
-        # print("Tempo medio de espera", temp_med_fila)
-        # print("Comp. medio da fila", comp_med_fila)
-        # print("Utilizacao do servico", utilizacao_serv)
-        # print("Tempo de simulacao", self.simulador.tempo)
-        # print("Numero de clientes atendidos", self.atendidos)
-        # print("Numero de clientes na fila", len(self.espera))
-
         strings = []
-        strings.append("Tempo medio de espera - " + str(temp_med_fila))
-        strings.append("Comp. medio da fila - " + str(comp_med_fila))
-        strings.append("Utilizacao do servico - " + str(utilizacao_serv))
-        strings.append("Tempo de simulacao - " + str(self.simulador.tempo))
+        strings.append("Tempo medio de espera - " + "{:.5f}".format(temp_med_fila))
+        strings.append("Comp. medio da fila - " + "{:.5f}".format(comp_med_fila))
+        strings.append("Utilizacao do servico - " + "{:.5f}".format(utilizacao_serv))
+        strings.append("Tempo de simulacao - " + "{:.5f}".format(self.simulador.tempo))
         strings.append("Numero de clientes atendidos - " + str(self.atendidos))
         strings.append("Numero de clientes na fila - " + str(len(self.espera)))
 
