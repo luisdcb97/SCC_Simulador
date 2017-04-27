d11b7df47a5b9dc69be3d3012aa238249ffdddd5
SPECTER\Luis David <luisdcb97@gmail.com>
2017-04-27 16:30:15 +0100

    Altera chegada da peca

--- Peca.py
+++ Peca.py
@@ -3,12 +3,14 @@
 
 import rand_generator
 
+
 class Peca:
     """
     Classe que representa uma peca
     """
 
-    def __init__(self, tipo: int, semente: int, stream: int, media: float, metodo=None, nome: str = "", custo: float = 0, seed_aleatoria: bool=False):
+    def __init__(self, tipo: int, semente: int, stream: int, media: float, metodo=None, nome: str = "",
+                 custo: float = 0, seed_aleatoria: bool = False):
         self.tipo = tipo
         self.nome = nome
         self.custo = custo
@@ -16,31 +18,25 @@ class Peca:
         self.semente = semente
         self.stream = stream
 
-        rand_generator.randst(semente, stream, seed_aleatoria)
-
         self.media = media
         self.metodo = metodo
+        self.seed_aleatoria = seed_aleatoria
+
+    def get_chegada(self):
         if self.metodo is None:
-            self.chegada = self.media
+            return self.media
         else:
-            self.chegada = metodo(self.stream, self.media)
+            return self.metodo(self.stream, self.media)
 
     def altera_media(self, media):
         self.media = media
-        if self.metodo is None:
-            self.chegada = self.media
-        else:
-            self.chegada = self.metodo(self.stream, self.media)
 
-    def altera_metodo(self, metodo = None):
+    def altera_metodo(self, metodo=None):
         self.metodo = metodo
-        if self.metodo is None:
-            self.chegada = self.media
-        else:
-            self.chegada = metodo(self.stream, self.media)
 
     def altera_aleatoriedade(self, altera: bool):
         rand_generator.randst(self.semente, self.stream, altera)
+        self.seed_aleatoria = altera
 
     def __str__(self):
         return self.nome
